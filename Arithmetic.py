088b411331a4408db2735a2d00e03bcbb72f7f87
sanket09c <sanket09c@gmail.com>
2025-05-16 01:42:36 +0530

    Add files via upload

---
+++ Arithmetic.py
@@ -0,0 +1,19 @@
+def Add(no1, no2):
+    ans = 0
+    ans  = no1 + no2
+    return ans
+
+def Mul(no1, no2):
+    ans = 0
+    ans  = no1 * no2
+    return ans
+
+def Sub(no1, no2):
+    ans = 0
+    ans  = no1 - no2
+    return ans
+
+def Div(no1, no2):
+    ans = 0
+    ans  = no1 / no2
+    return ans
